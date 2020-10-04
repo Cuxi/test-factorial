1e77291b575277a8c04e9e1434c4ec34da93e55a
cuxi <marferyan@gmail.com>
2020-10-05 00:41:20 +0200

    Update http-common.js
    Setting url

--- app/javascript/http-common.js
+++ app/javascript/http-common.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://test-factorial.herokuapp.com/api/v1",
+  baseURL: "http://localhost:3000/api/v1",
   headers: {
     "Content-type": "application/json",
     'X-CSRF-TOKEN': `${localStorage.getItem('csrf')}`,
